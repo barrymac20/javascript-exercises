b5650ea160156400e5cbf89cb206ff5d05918660
Barry McCullough <barrymac20@gmail.com>
2022-11-15 14:33:51 +0000

    Update javascript file to pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+//My solution
+// const reverseString = function(string) {
+//   const splitArray = string.split("");  //split string into array with string.split()
+//   const reverseArray = splitArray.reverse(); //reverse array with array.reverse()
+//   const newString = reverseArray.join("");//convert array to string with array.join()
+//   return newString;
+// };
 
-};
+const reverseString = function(string) {
+  return string.split('').reverse().join('');
+ };
 
 // Do not edit below this line
 module.exports = reverseString;
