89cc30f54031f1a1fe570b7555e8b1dd8ae89bf9
Barry McCullough <barrymac20@gmail.com>
2022-11-15 17:10:28 +0000

    Update javascript files to pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
 
+const removeFromArray = function(myArray, ...itemsToRemove) { //... before argument allow multiple argument
+  
+  itemsToRemove.forEach(function(item) { //for loop for each item in itemsToRemove // could use for (const item of itemsToRemove) {
+    const index = myArray.indexOf(item); //get the index of the item in myArray
+    if (index > -1) //if index is zero or greater
+      myArray.splice(index, 1); //splice at that index and remove one
+  });
+  return myArray
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
