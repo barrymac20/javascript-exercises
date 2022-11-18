edbdacfe2e7818a085e82c9f7abf7e2f540f564a
Barry McCullough <barrymac20@gmail.com>
2022-11-18 12:48:45 +0000

    Update javascript file to pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,23 @@
-const ftoc = function() {
+const ftoc = function(fTemp) {
+  let num = (fTemp-32)*5/9
+  return Math.round(num*10)/10
 
 };
 
-const ctof = function() {
+const ctof = function(cTemp) {
+  let num = (cTemp*9/5)+32
+  return Math.round(num*10)/10
 
 };
+
+//TOP Solution
+// const ftoc = function(f) {
+//   return Math.round((f - 32) * (5/9) * 10) / 10;
+//  };
+ 
+//  const ctof = function(c) {
+//    return Math.round(((c * 9/5) + 32) * 10) / 10;
+//  };
 
 // Do not edit below this line
 module.exports = {
