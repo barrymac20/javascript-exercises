4eba3db862b36ea393f87d53ddfae2a732c92ad0
Barry McCullough <barrymac20@gmail.com>
2023-03-15 16:12:10 +0000

    Add function to make all tests pass

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
-
+//My solution
+const fibonacci = function(num) {
+  let array = [];
+  if (num<0) {return "OOPS"};
+  for (let i=0; i<num+1; i++){
+    if (i == 0 || i == 1 ){
+      array.push(i)
+    } else{
+    array.push(array[i-2]+array[i-1])
+    }  
+  }
+  return(array[num])
 };
+
+//Odin Project Solution
+// const fibonacci = function(count) {
+//   if (count < 0) return "OOPS";
+//   if (count === 0) return 0;
+//   let a = 0;
+//   let b = 1;
+//   for (let i = 1; i < count; i++) {
+//     const temp = b;
+//     b = a + b;
+//     a = temp;
+//   }
+//   return b;
+// };
 
 // Do not edit below this line
 module.exports = fibonacci;
