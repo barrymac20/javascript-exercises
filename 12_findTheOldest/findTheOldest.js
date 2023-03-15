6f46e7182da1e6914aa1091958ad2dc99adde968
Barry McCullough <barrymac20@gmail.com>
2023-03-15 23:09:35 +0000

    Add function to pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  
+  people.sort(function(a,b) {
+    if(!a.yearOfDeath){a.yearOfDeath = new Date().getFullYear()}else{a.yearOfDeath};
+    if(!b.yearOfDeath){b.yearOfDeath = new Date().getFullYear()}else{b.yearOfDeath};
+    return (a.yearOfDeath-a.yearOfBirth) > (b.yearOfDeath-b.yearOfBirth) ? -1:1;
+  })
 
+  return people[0]
 };
 
 // Do not edit below this line
