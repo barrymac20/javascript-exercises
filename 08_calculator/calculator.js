d2ffa76bcdeea89326d892eeee9254620689bfb5
Barry McCullough <barrymac20@gmail.com>
2023-03-15 23:12:18 +0000

    Add function to pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+  return myArray.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
+const multiply = function(myArray) {
+  return myArray.reduce((total, current) => total * current, 1)
+};
+
+const power = function(a,b) {
+	return a**b
 
 };
 
-const power = function() {
-	
+// const factorial = function(num) {
+//   if (num===0) {
+//     return 1
+//   };
+  
+//   let fact = 1;
+//   for (let i=num; i>0; i--) {
+//     fact *= i;
+//   }
+//   return fact;
+// };
+
+//recursion solution
+const factorial = function(num) {
+  if (num===0) {
+    return 1
+  };
+  return num * factorial (num-1);
 };
 
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
