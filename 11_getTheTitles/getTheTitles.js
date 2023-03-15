b0b55c131b690997c0bb926f9b6d65a9786db200
Barry McCullough <barrymac20@gmail.com>
2023-03-15 16:49:08 +0000

    Add function to pass test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
+//My working solution
+// const getTheTitles = function(books) {
+//   let booksArray = [];
+//   for(let i=0; i<books.length; i++){
+//     booksArray.push(books[i].title)
+//   }
+//   //console.log(booksArray)
+//   return booksArray
+// };
 
+//Odin project solution - much better
+const getTheTitles = function(array) {
+  return array.map(book => book.title);
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
